550963a4693bd0177b8a4852989a28d34953b250
Johan Schierbeck <johan@schilie.se>
2014-11-10 09:54:11 +0100

    Minor update and README.md file

---
+++ README.md
@@ -0,0 +1,13 @@
+#Structure
+
+[docroot]
+
+	[assets]
+		[css]
+		[fonts]
+		[images]
+		[javascripts]
+		[stylesheets]
+	gulpfile.js
+	package.json
+	README.md (this file)
--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,36 @@
 var p = require('./package.json'),
-		gulp = require('gulp'),
-		sass = require('gulp-sass'),
-		uglify = require('gulp-uglify'),
-		jshint = require('gulp-jshint'),
-		concat = require('gulp-concat'),
-		notify = require('gulp-notify'),
-		plumber = require('gulp-plumber'),
-		stylish = require('jshint-stylish'),
-		minifycss = require('gulp-minify-css'),
-		browserSync = require('browser-sync'),
-		autoprefixer = require('gulp-autoprefixer'),
-		gutil = require('gulp-util');
+	gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	uglify = require('gulp-uglify'),
+	jshint = require('gulp-jshint'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber'),
+	stylish = require('jshint-stylish'),
+	minifycss = require('gulp-minify-css'),
+	browserSync = require('browser-sync'),
+	autoprefixer = require('gulp-autoprefixer'),
+	gutil = require('gulp-util');
 
+// Pretty sass errors
 var sassError = function (err) {  
   gutil.beep();
   gutil.log('\n' + gutil.colors.bgRed.bold.white('\n\n (╯°□°)╯ SASS ERROR! \n') + gutil.colors.red.bgWhite('\n\n'+err) + gutil.colors.bgRed.bold.white('\n'));
 };
-
 gutil.log('\n' + gutil.colors.bgBlue.bold('\n\n ♪ ' + p.name + '\n > ' + p.version +'\n') + '\n');
+
 
 var target = {
 	sass_src : 'docroot/assets/stylesheets/style.scss',
 	css_dest : 'docroot/assets/css/',
 	js_lint_src : [
-		'docroot/assets/js/script.js'
+		'docroot/assets/javascripts/script.js'
 	],
 	js_concat_src : [
-		'docroot/assets/js/jquery.js',
-		'docroot/assets/js/script.js'
+		'docroot/assets/javascripts/jquery.js',
+		'docroot/assets/javascripts/script.js'
 	],
-	js_dest : 'docroot/assets/js'
+	js_dest : 'docroot/assets/javascripts'
 };
 
 /**
@@ -71,7 +72,7 @@ gulp.task('js-lint', function() {
  * BROWSER SYNC
  */
 gulp.task('browser-sync', function() {
-	browserSync.init(['docroot/assets/css/*.css', 'docroot/assets/js/*.js'], {
+	browserSync.init(['docroot/assets/css/*.css', 'docroot/assets/javascripts/*.js'], {
 		// proxy: "local.reportage.se"
 		server: {
 			baseDir: "docroot"
@@ -87,7 +88,7 @@ gulp.task('default', function() {
 	gulp.watch('docroot/assets/stylesheets/*.scss', function() {
 		gulp.run('sass');
 	})
-	gulp.watch('docroot/assets/js/*.js', function() {
+	gulp.watch('docroot/assets/javascripts/*.js', function() {
 		gulp.run('js-lint');
 	});
 });
