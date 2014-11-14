dbfe8e56393d115c78637a4e44eb030defa96855
Johan Schierbeck <johan@schilie.se>
2014-11-14 16:24:28 +0100

    cleaned up code

--- docroot/assets/javascripts/script.js
+++ docroot/assets/javascripts/script.js
@@ -1,1 +1,11 @@
-console.log('Dallas'),console.log('Sallad');
+console.log('Dallas'),console.log('Sallad'),console.log('Hej å h'),console.log('Nä'),console.log('data');
+
+
+$(document).ready(function () {
+
+	console.log('jQuery up n runnin!');
+	console.log('jQuery up n runnin!');
+
+});
+
+console.log('asdasdsa');
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
-var p = require('./package.json'),
-	gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	uglify = require('gulp-uglify'),
-	jshint = require('gulp-jshint'),
-	concat = require('gulp-concat'),
-	notify = require('gulp-notify'),
-	plumber = require('gulp-plumber'),
-	stylish = require('jshint-stylish'),
-	minifycss = require('gulp-minify-css'),
-	browserSync = require('browser-sync'),
-	autoprefixer = require('gulp-autoprefixer'),
-	gutil = require('gulp-util');
+// Sources
+var root_src = 'docroot',
+		assets_src = root_src + '/assets',
+		sass_src = assets_src + 	'/stylesheets',
+		css_src = assets_src + 	'/css',
+		js_src = assets_src + '/javascripts',
+		img_src = assets_src + '/images';
+
+var p = require('./package.json'), // Our project file
+		gulp = require('gulp'), // Gulp!
+		sass = require('gulp-sass'), // Sass compiler
+		gutil = require('gulp-util'), // Style log messages
+		rename = require('gulp-rename'), // Rename files
+		uglify = require('gulp-uglify'), // Make js tiny and ugly
+		jshint = require('gulp-jshint'), // JavaScript validator (really nags!)
+		gulp_concat = require('gulp-concat'), // Mash multiple files into one
+		notify = require('gulp-notify'), // System notifications
+		plumber = require('gulp-plumber'), // Keeps gulp running on errors
+		stylish = require('jshint-stylish'), // Better lookin jsHint messages
+		browserSync = require('browser-sync'), // No need to refresh browser for styling changes
+		minifycss = require('gulp-minify-css'), // Minify CSS
+		autoprefixer = require('gulp-autoprefixer'); // No more prefixes for different browsers! :-D
 
 // Pretty sass errors
 var sassError = function (err) {  
@@ -19,85 +28,62 @@ var sassError = function (err) {
 };
 gutil.log('\n' + gutil.colors.bgBlue.bold('\n\n ♪ ' + p.name + '\n > ' + p.version +'\n') + '\n');
 
-
-var target = {
-	sass_src : 'docroot/assets/stylesheets/style.scss',
-	css_dest : 'docroot/assets/css/',
-	js_lint_src : [
-		'docroot/assets/javascripts/script.js'
-	],
-	js_concat_src : [
-		'docroot/assets/javascripts/jquery.js',
-		'docroot/assets/javascripts/script.js'
-	],
-	js_dest : 'docroot/assets/javascripts'
-};
-
 /**
  * SASS
  */
 gulp.task('sass', function() {
-	gulp.src(target.sass_src)
-		
-		.pipe(plumber({
+	gulp.src(sass_src + '/style.scss')
+		.pipe(plumber({ // Keeps gulp running on errors
       errorHandler: sassError
-    }))											// Keeps gulp running on errors
+    }))
 		.pipe(sass({
 			sourceComments : 'normal' // Bättre felmeddelanden
 		}))
 		.pipe(autoprefixer( // Definiera vilka browsers som bör prefixas för
-			'last 2 version',
-			'> 1%',
-			'ie 8',
-			'ie 9',
-			'Opera',
-			'ios 6',
-			'android 4'
+			'last 2 version', '> 1%', 'ie 8', 'ie 9', 'Opera', 'ios 6', 'android 4'
 		))
 		.pipe(minifycss())
-		.pipe(gulp.dest(target.css_dest))
+		.pipe(gulp.dest(css_src))
 		.pipe(notify({title: 'SUCESS!', message : 'Sass was compiled.'}));
 });
 
 /**
- * JS LINT
+ * JAVASCRIPT
  */
-gulp.task('js-lint', function() {
-	gulp.src(target.js_lint_src)
+gulp.task('js', function() {
+	gulp.src(js_src + '/script.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter(stylish));
-});
-
-gulp.task('js-compress', function() {
-  gulp.src('docroot/assets/javascripts/*.js')
-    .pipe(uglify('script.min.js', {
-    	outSourceMap: true
-    }))
-    .pipe(gulp.dest('docroot/assets/javascripts'))
+  gulp.src([
+  	js_src + '/jquery.js',
+  	js_src + '/script.js'
+  	])
+  .pipe(gulp_concat('script.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(js_src));
 });
 
 /**
  * BROWSER SYNC
  */
 gulp.task('browser-sync', function() {
-	browserSync.init(['docroot/assets/css/*.css', 'docroot/assets/javascripts/*.js', 'docroot/*.html'], {
-		// proxy: "local.mysite.com"
-		server: {
-			baseDir: "docroot"
-		}
-	})
+  browserSync({
+    files: root_src + "/**/*",
+    server: {
+      baseDir: "docroot"
+    }
+  });
 });
 
 /**
- * DEFAULT TASK
+ * TASKS
  */
 gulp.task('default', function() {
-	gulp.run('sass', 'browser-sync', 'js-lint');
-	gulp.watch('docroot/assets/stylesheets/*.scss', function() {
+	gulp.run('sass', 'js', 'browser-sync');
+	gulp.watch( sass_src + '/**/*.scss', function() {
 		gulp.run('sass');
 	})
-	gulp.watch('docroot/assets/javascripts/*.js', function() {
-		gulp.run('js-lint');
-		//gulp.run('js-compress');
+	gulp.watch( js_src + '/script.js', function() {
+		gulp.run('js');
 	});
 });
